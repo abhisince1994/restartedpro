1c442abd14803409ba313b82823d7992e9d7c3f6
abhisince1994 <abhijeetsapkal2@gmail.com>
2024-10-05 11:27:56 +0530

    404,routes filtering paths

--- app.js
+++ app.js
@@ -3,33 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Use body-parser to parse form data
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// Route to show the form with two input fields: title and size
-app.use('/add-product', (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" placeholder="Product Title">
-      <input type="text" name="size" placeholder="Product Size">
-      <button type="submit">Add Product</button>
-    </form>
-  `);
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
-// Route to handle form submission
-app.post('/product', (req, res, next) => {
-  // Log the parsed form data (title and size) to the console
-  console.log(req.body); // { title: 'user-input-title', size: 'user-input-size' }
-  res.redirect('/');
-});
-
-// Default route
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000);
